0ecf0ee42f3f54fe1f43e6a3598b27efe62161f2
andrew.k20 <andrew.k20@student.parishepiscopal.org>
2018-09-12 14:45:43 -0500

    added lab 2

---
+++ Lab02-BlueJ/Lab02v100.java
@@ -0,0 +1,19 @@
+public class Lab02v100
+{
+public static void main(String[] args)
+{
+    System.out.println("Lab 02, 100 Point Version");
+    System.out.println();
+    System.out.println();
+    System.out.print("Measure today's knowledge ");
+    System.out.println("by yesterday's confusion.");
+    System.out.println();
+    System.out.println("Bewilderment + Exposure = Obvious");
+    System.out.println();
+    System.out.println("# ##### #####    ##### ##### # #   # ###### #####");
+    System.out.println("# #   # #   #    #   # #   # # ##  #   #    #");
+    System.out.println("# #   # #   #    ##### #   # # # # #   #    #####");
+    System.out.println("# #   # #   #    #     #   # # #  ##   #        #");
+    System.out.println("# ##### #####    #     ##### # #   #   #    #####");
+}
+} 
---
+++ Lab02-BlueJ/Lab02v80.java
@@ -0,0 +1,14 @@
+public class Lab02v80
+{
+public static void main(String[] args)
+{
+    System.out.println("Lab 02, 80 Point Version");
+    System.out.println();
+    System.out.println();
+    System.out.print("Measure today's knowledge ");
+    System.out.println("by yesterday's confusion.");
+    System.out.println();
+    System.out.println("Bewilderment + Exposure = Obvious");
+    System.out.println();
+}
+} 
